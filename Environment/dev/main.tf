b51012b4ee265418da3bc4061bfbb5276f0b1b10
mak87ec-art <mak87ec@gmail.com>
2026-08-08 06:57:59 -0700

    workflowcode added

---
+++ Environment/dev/main.tf
@@ -0,0 +1,25 @@
+module "resource_grp" {
+  source = "../../Modules/azurerm_resource_group"
+  rgs    = var.rgs
+}
+module "virtual_network" {
+  depends_on = [module.resource_grp]
+  source     = "../../Modules/azurerm_virtual_network"
+  vnets      = var.vnets
+}
+module "subnet" {
+  depends_on = [module.virtual_network]
+  source     = "../../Modules/azurerm_subnet"
+  subnets    = var.subnets
+}
+module "bastion_host" {
+  depends_on = [module.subnet]
+  source     = "../../Modules/azurerm_bastion"
+  BHs        = var.BHs
+}
+module "key_vault" {
+    depends_on = [ module.resource_grp ]
+    source = "../../Modules/azurerm_key_vault"
+    kv = var.kv
+  
+}
---
+++ Environment/dev/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "5.0.1"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "mak"
+    storage_account_name = "storageforstate007"
+    container_name = "state-container"
+    key = "dev-state"
+    
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
