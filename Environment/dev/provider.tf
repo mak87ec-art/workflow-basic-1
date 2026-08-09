030705ec622b76dc854b1f6c395c0a692cb1a451
mak87ec-art <mak87ec@gmail.com>
2026-08-09 03:55:37 -0700

    pipeline added

--- Environment/dev/provider.tf
+++ Environment/dev/provider.tf
@@ -9,7 +9,7 @@ terraform {
     resource_group_name = "mak"
     storage_account_name = "storageforstate007"
     container_name = "state-container"
-    key = "dev-state"
+    key = "test-state"
     
   }
 }
